8152b228986760e88a28fd331df660d22f8b2964
abhinandpn <abhinandpn4067@gmail.com>
2023-11-12 23:38:51 +0530

    updated and delete function added

--- todo/curd.go
+++ todo/curd.go
@@ -148,6 +148,13 @@ func UpdateTodo(client *firestore.Client) func(c *gin.Context) {
 func DeleteTodo(client *firestore.Client) func(c *gin.Context) {
 
 	return func(c *gin.Context) {
+		id := c.Param("id")
+		_, err := client.Collection(model.TODO_COLLECTION).Doc(id).Delete(c)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, "")
+			return
+		}
 
+		c.JSON(http.StatusOK, "succesfully deleted")
 	}
 }
